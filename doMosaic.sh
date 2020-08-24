5c226ae8c00e39c6995063fa4989c041ca0eb859
CharlesTiarini <charlestiarini@gmail.com>
2020-08-24 11:22:01 -0300

    Update doMosaic.sh

--- doMosaic.sh
+++ doMosaic.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-NOME='totalNDVIqV3'
+for NOME in '1500_1_99_NDVIq' '1500_1_99_CVP' '1500_5_95_NDVIq' '1500_5_95_CVP' '1500_10_90_NDVIq' '1500_10_90_CVP' '1500_percentil' '2400_1_99_NDVIq' '2400_1_99_CVP' '2400_5_95_NDVIq' '2400_5_95_CVP' '2400_10_90_NDVIq' '2400_10_90_CVP' '2400_percentil';
+do
+    echo $NOME
+	gdalbuildvrt $NOME.vrt $NOME*.tif -srcnodata 0 -vrtnodata 0
+	gdal_translate $NOME.vrt $NOME.tif -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES -a_nodata 0
+	#gdaladdo -ro $NOME.tif 2 4 8
+	echo $NOME 'is done'
+done
 
-gdalbuildvrt $NOME.vrt *.tif -srcnodata 0 -vrtnodata 0
-gdal_translate $NOME.vrt $NOME.tif -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES -a_nodata 0
-gdaladdo -ro $NOME.tif 2 4 8
